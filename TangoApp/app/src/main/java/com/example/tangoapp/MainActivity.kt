a4b458772343e920db64ad958287c0e7a73bc089
sakuradadaisuke <sakuradaasaku@gmail.com>
2022-02-27 18:30:43 +0900

    保存ボタンのバリデーションの実装

--- TangoApp/app/src/main/java/com/example/tangoapp/MainActivity.kt
+++ TangoApp/app/src/main/java/com/example/tangoapp/MainActivity.kt
@@ -8,6 +8,7 @@ import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
--- TangoApp/app/src/main/java/com/example/tangoapp/WordAdditionActivity.kt
+++ TangoApp/app/src/main/java/com/example/tangoapp/WordAdditionActivity.kt
@@ -3,15 +3,48 @@ package com.example.tangoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.tangoapp.databinding.ActivityWordAdditionBinding
 
 
 class WordAdditionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWordAdditionBinding
+    private var englishWord = ""
+    private var japaneseWord = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWordAdditionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.englishInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                englishWord = binding.englishInput.text.toString()
+                binding.saveButton.isEnabled = !(englishWord == "" || japaneseWord == "")
+            }
+        })
+
+        binding.japaneseInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                japaneseWord = binding.japaneseInput.text.toString()
+                binding.saveButton.isEnabled = !(englishWord == "" || japaneseWord == "")
+            }
+        })
+
+
 
         binding.mainBackButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
